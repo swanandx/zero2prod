1641262eb68e849d7080fb4000c9c4c66f441d2e
swanandx <73115739+swanandx@users.noreply.github.com>
2022-03-03 17:37:14 +0530

    Using random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listner)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spwan_app();
+    let address = spwan_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(address + "/health_check")
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,7 +15,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spwan_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spwan_app() -> String {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listner.local_addr().unwrap().port();
+    let server = zero2prod::run(listner).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{port}")
 }
