8b9c09848936be19fb388e26b576488f453bfea3
swanandx <73115739+swanandx@users.noreply.github.com>
2022-03-31 18:26:28 +0530

    changed project structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 fn spwan_app() -> String {
     let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listner.local_addr().unwrap().port();
-    let server = zero2prod::run(listner).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listner).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{port}")
 }
