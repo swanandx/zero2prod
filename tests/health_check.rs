e2a4608fbbedd477b2e8548b8c4dcee8f7c57fc8
swanandx <73115739+swanandx@users.noreply.github.com>
2022-03-31 18:09:29 +0530

    finished Database setup

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,13 @@
 use std::net::TcpListener;
 
+fn spwan_app() -> String {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listner.local_addr().unwrap().port();
+    let server = zero2prod::run(listner).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{port}")
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spwan_app();
@@ -15,10 +23,46 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spwan_app() -> String {
-    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listner.local_addr().unwrap().port();
-    let server = zero2prod::run(listner).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{port}")
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spwan_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=Swanand%20Mulay&email=swanandx%40github.com";
+    let response = client
+        .post(app_address + "/subscriptions")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute reqwest.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spwan_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=swanand%20mulay", "missing the email"),
+        ("email=swanandx@github.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{app_address}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute reqwest.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {error_message}."
+        );
+    }
 }
